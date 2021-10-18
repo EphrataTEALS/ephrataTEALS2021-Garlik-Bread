f82d43c49d2faa4f4291de0a2ee972bd53f9ae5b
Garlik-Bread <90467683+Garlik-Bread@users.noreply.github.com>
2021-10-18 16:40:17 +0000

    Modified CodespacesTest.java

---
+++ labs/EvenOdd/EvenOdd.java
@@ -0,0 +1,22 @@
+/**
+ * Prints out if number is even or odd to the console.
+ * 
+ * @author Jacob
+ */
+
+ package labs.EvenOdd;
+
+ public class EvenOdd {
+
+    public static void main(String[] args) {
+        int user = 0;
+
+        if ((user % 2) == 0) {
+            System.out.println(user+ " is Even.");
+         } else {
+         System.out.println(user+ " is Odd.");
+         }
+
+
+    }
+ }
---
+++ labs/EvenOdd/PositiveNegative.java
@@ -0,0 +1,16 @@
+package labs.EvenOdd;
+
+public class PositiveNegative {
+    
+    public static void main(String[] args) {
+        int user  = 0;
+        
+        if (user > 0 ) {
+            System.out.println("Positive");
+        } else if (user < 0) {
+            System.out.println("Negative");
+        } else {
+            if (user == 0); {
+            System.out.println(user + " is neither positive nor negative."); 
+    }}
+}}
